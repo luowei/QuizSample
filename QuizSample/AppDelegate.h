b7400d2aa1f5325b0416712f19cc38ffd5ccce70
luowei <luowei010101@gmail.com>
2014-06-05 13:46:57 +0800

    Initial Commit

---
+++ QuizSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QuizSample
+//
+//  Created by luowei on 14-6-5.
+//  Copyright (c) 2014年 rootls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QuizSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QuizSample
+//
+//  Created by luowei on 14-6-5.
+//  Copyright (c) 2014年 rootls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
